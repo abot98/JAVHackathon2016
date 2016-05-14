cc102a688afc5c8f66270874cc85dad1d7cf20f1
Violet <vkilly@gmail.com>
2016-05-14 11:40:18 -0400

    Added permissions

--- AtomHacks/gen/com/example/atomhacks/R.java
+++ AtomHacks/gen/com/example/atomhacks/R.java
@@ -24,13 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int button1=0x7f080002;
-        public static final int editText1=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int button1=0x7f080006;
+        public static final int editText1=0x7f080005;
+        public static final int email=0x7f080000;
+        public static final int newAccount=0x7f080002;
+        public static final int password=0x7f080001;
+        public static final int submitLogin=0x7f080003;
+        public static final int textView1=0x7f080004;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int login=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int sign_up=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- AtomHacks/src/com/example/atomhacks/Main.java
+++ AtomHacks/src/com/example/atomhacks/Main.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.firebase.client.Firebase;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
