394b6723023e06fbb9f13be0cb714c5f5f47e995
Arun Bishop <arunleob@gmail.com>
2016-05-14 11:25:17 -0400

    Updated Main.java

--- AtomHacks/src/com/example/atomhacks/Main.java
+++ AtomHacks/src/com/example/atomhacks/Main.java
@@ -1,15 +1,43 @@
 package com.example.atomhacks;
 
 import android.os.Bundle;
+
+import com.firebase.client.Firebase;
+
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main extends Activity {
 
+	private EditText inputText;
+	private Button sendBtn;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //Establishes firebase (server)
+        Firebase.setAndroidContext(this);
+        
+        //Database class for server comms
+        final DatabaseInterface databaseInterface = new DatabaseInterface();
+        
+        inputText = (EditText) findViewById(R.id.editText1);
+        sendBtn = (Button) findViewById(R.id.button1);
+        //Sends simple info to server
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+					databaseInterface.sendMessage(inputText.getText().toString());	
+					inputText.setText("");
+			}
+		});
     }
 
 
